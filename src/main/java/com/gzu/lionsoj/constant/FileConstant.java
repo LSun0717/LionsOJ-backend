98dc4a35c7154530588a9931d4de52ee826d9fb3
LSun <LSun0717@163.com>
2023-12-26 20:50:53 +0800

    project init

---
+++ src/main/java/com/gzu/lionsoj/constant/FileConstant.java
@@ -0,0 +1,14 @@
+package com.gzu.lionsoj.constant;
+
+/**
+ * 文件常量
+ *
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://gzu.icu";
+}
---
+++ src/main/java/com/gzu/lionsoj/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,30 @@
+package com.gzu.lionsoj.model.dto.postfavour;
+
+import com.gzu.lionsoj.model.dto.post.PostQueryRequest;
+import com.gzu.lionsoj.common.PageRequest;
+
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
